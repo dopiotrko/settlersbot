6f7dca283740f132f384eaecc97a79c69785cdb3
Piotr Kowalczyk <dopiotrko@o2.pl>
2020-06-10 22:55:54 +0200

    new types for gui added

--- my_types.py
+++ my_types.py
@@ -57,3 +57,55 @@ class Mode(Enum):
     play = 1
     teach_co = 2
     teach_delay = 3
+
+
+class Action:
+    def __init__(self, *args, **kwargs):
+        self.no = kwargs['no']
+        self.type = kwargs['type']
+        self.delay = kwargs['delay']
+        self.generals = [General(self, **gen) for gen in kwargs['generals']]
+
+    def get_data_for_table(self, attr):
+        if attr == 'generals':
+            return ', '.join('{} ({})'.format(gen.type, gen.id) for gen in self.generals)
+        else:
+            return getattr(self, attr, False)
+
+    def set_data_from_table(self, attr, value):
+        setattr(self, attr, value)
+
+    def get_generals(self):
+        return self.generals
+
+
+class General:
+    def __init__(self, *args, **kwargs):
+        self.keys = ["recruit", "bowmen", "militia",
+                     "cavalry", "longbowman", "soldier",
+                     "crossbowman", "elite_soldier", "cannoneer"]
+        self.id = kwargs.get('id', 0)
+        self.type = kwargs.get('type', 'empty')
+        self.delay = kwargs.get('delay', 0)
+        self.preset = kwargs.get('preset', False)
+        self.init = kwargs.get('init', False)
+        self.army = kwargs.get('army', {})
+        self.retreat = kwargs.get('retreat', False)
+        self.relative_coordinates = kwargs.get('relative_coordinates', None)
+        self.drag = kwargs.get('drag')
+        self.parent = args[0]
+        # TODO how to set this? from conf file?
+        self.capacity = 250
+
+    def get_units(self, key_or_index):
+        key = self._key_to_index(key_or_index)
+        return self.army[key]
+
+    def set_units(self, key_or_index, value):
+        key = self._key_to_index(key_or_index)
+        self.army[key] = value
+
+    def _key_to_index(self, key_or_index):
+        assert isinstance(key_or_index, int) or isinstance(key_or_index, str)
+        key = key_or_index if isinstance(key_or_index, str) else self.keys[key_or_index]
+        return key
